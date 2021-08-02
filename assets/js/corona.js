135c9dd033b9b75ded79b1f544851148986661bd
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-02 13:11:30 +0200

    untrack data from corona api to index.html

--- assets/js/corona.js
+++ assets/js/corona.js
@@ -1,14 +1,56 @@
-fetch("https://covid-19-data.p.rapidapi.com/country?name=Germany", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-key": "c627d0f2d2msh9ba409e5b8c495dp17c448jsn5469ec3ad079",
-        "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
-    }
-})
-.then(response => response.json())
-.then(data => console.dir(data))
-.then(console.log(data.confirmed))
-.catch(err => console.error(err));
+// fetch("https://covid-19-data.p.rapidapi.com/country?name=Germany", {
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-key": "c627d0f2d2msh9ba409e5b8c495dp17c448jsn5469ec3ad079",
+//         "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+//     }
+// })
+// .then(response => response.json())
+// .then(data => console.dir(data))
+// .then(console.log(data))
+// .catch(err => console.error(err));
+
+
+
+
+async function getData() {
+
+    const response = await fetch("https://covid-19-data.p.rapidapi.com/country?name=Germany", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "c627d0f2d2msh9ba409e5b8c495dp17c448jsn5469ec3ad079",
+            "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+        }
+    });
+
+    const data = await response.json();
+    const options = await data[0];
+    const {
+        country,
+        confirmed,
+        deaths,
+        recovered,
+        lastUpdate
+    } = options;
+
+    
+
+    document.getElementById('country').textContent = country;
+    document.getElementById('confirmed').textContent = confirmed;
+    document.getElementById('death').textContent = deaths;
+    document.getElementById('recovered').textContent = recovered;
+    document.getElementById('update').textContent = lastUpdate;
+}
+
+getData();
+
+
+
+
+
+
+
+
 
 
 // code: "DE"
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,13 @@
 window.onload = function() {
     $(".main-img").hide().fadeIn(4000);
 };
+
+$('.corona-data').hide();
+
+$('.corona-btn').click(function(){
+   $('.corona-data').slideToggle('slow');
+//    $('.corona-btn').addClass("mb-3");
+});
+
+
+

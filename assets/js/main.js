5e62ad6a500a6a1494875c2eb21a9b92f8014fa4
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-07 16:20:48 +0200

    add description to all services in services.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,49 @@
-gsap.from(".main-img", { duration: 5, ease: "circ.out", y: -500});
+gsap.from(".main-img", {
+    duration: 5,
+    ease: "circ.out",
+    y: -500
+});
 
 // window.onload = function() {
 //     $(".main-img").hide().fadeIn(4000);
-    
+
 // };
 
 $('#corona-news').hide();
 $('#stats-data').hide();
 
 
-$('#corona-btn').click(function(){
-   $('#corona-news').slideToggle('slow');
-   
+$('#corona-btn').click(function () {
+    $('#corona-news').slideToggle('slow');
+
 });
 
-$('#stats-btn').click(function(){
+$('#stats-btn').click(function () {
     $('#stats-data').slideToggle('slow');
-    
- });
+
+});
 
 
 
- $(".service-desc").hide();
-
-
-
+$(".service-desc").hide();
 
 
 
 
- $(".services-link").click(function(){
+
+$(".services-link").on('click', function () {
     $('.service-short-desc > p, h5, .services-img').hide();
     $(".service-desc").fadeIn('slow');
     $('.col-toggle').toggleClass('col-lg-8 col-10');
- });
+});
 
 
 
-
-
- $(".back-btn").click(function(){
+$(".back-btn").click(function () {
     $(".service-desc").hide();
     $('.service-short-desc > p, h5, .services-img').show();
     $('.col-toggle').removeClass('col-lg-8 col-10');
- });
+});
+
+
+

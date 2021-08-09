3a63e347fc154875bb37204accff867c744b4ab9
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-09 11:28:35 +0200

    add job.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,11 @@ $(".service-desc").hide();
 
 
 
-$(".services-link").on('click', function () {
-    $('.service-short-desc > p, h5, .services-img').hide();
+$(".services-link").on('click', function(){
+    $(this)('.service-short-desc > p, h5, .services-img').hide();
     $(".service-desc").fadeIn('slow');
     $('.col-toggle').toggleClass('col-lg-8 col-10');
+    console.log(this);
 });
 
 
@@ -44,6 +45,37 @@ $(".back-btn").click(function () {
     $('.service-short-desc > p, h5, .services-img').show();
     $('.col-toggle').removeClass('col-lg-8 col-10');
 });
+
+
+gsap.set(".quickflip", {
+    transformStyle: "preserve-3d",
+    transformPerspective: 1000
+});
+gsap.set(".qf-card", {
+    transformStyle: "preserve-3d",
+    transformOrigin: "50% 50%",
+});
+gsap.set(".qf-back", {
+    rotationY: 180, rotationZ: 180
+});
+
+const timing = 1;
+
+const tl = gsap.timeline({ paused: true });
+tl.to(".qf-card", { rotationX: "+=180", duration: timing });
+tl.to(".quickflip", { z: 50, duration: timing/2, yoyo: true, repeat: 1 }, 0);
+
+gsap.utils.toArray(".quickflip").forEach(flip => {
+    flip.addEventListener("click", event => {
+      tl.play();
+      
+      
+    });
+    // flip.addEventListener("mouseenter", event => tl.play());
+    // flip.addEventListener("mouseleave", event => tl.reverse());
+});
+
+
 
 
 

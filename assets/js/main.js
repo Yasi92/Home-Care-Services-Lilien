d906472e722bdfef918f6bb18f4f1b9605fc75b2
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-09 16:43:35 +0200

    fix targeting each card on click in services.html, add style.scss, add flip cards to the job.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,12 +31,33 @@ $(".service-desc").hide();
 
 
 
-$(".services-link").on('click', function(){
-    $(this)('.service-short-desc > p, h5, .services-img').hide();
-    $(".service-desc").fadeIn('slow');
-    $('.col-toggle').toggleClass('col-lg-8 col-10');
-    console.log(this);
+// $(".services-link").on('click', function(){
+//     $('.service-short-desc > p, h5, .services-img').hide();
+//     $(".service-desc").fadeIn('slow');
+//     $('.col-toggle').toggleClass('col-lg-8 col-10');
+//     console.log(this);
+// });
+
+$(".services-link").on('click', function(event){
+    // $('.service-short-desc > p, h5, .services-img').hide();
+    const button = event.target;
+    const text = event.target.parentElement.nextElementSibling;
+    const clToggle = event.target.parentElement.parentElement;
+    const image = event.target.parentElement.parentElement.previousElementSibling;
+
+    console.log(image);
+
+    $(button).hide();
+    $(image).hide();
+    $(text).fadeIn('slow');
+    $(clToggle).toggleClass('col-lg-8 col-10');
+    
+    // $(this).hide()
+    // $(".service-desc").fadeIn('slow');
+    // $('.col-toggle').toggleClass('col-lg-8 col-10');
+    // console.log($(this));
 });
+
 
 
 
@@ -47,33 +68,6 @@ $(".back-btn").click(function () {
 });
 
 
-gsap.set(".quickflip", {
-    transformStyle: "preserve-3d",
-    transformPerspective: 1000
-});
-gsap.set(".qf-card", {
-    transformStyle: "preserve-3d",
-    transformOrigin: "50% 50%",
-});
-gsap.set(".qf-back", {
-    rotationY: 180, rotationZ: 180
-});
-
-const timing = 1;
-
-const tl = gsap.timeline({ paused: true });
-tl.to(".qf-card", { rotationX: "+=180", duration: timing });
-tl.to(".quickflip", { z: 50, duration: timing/2, yoyo: true, repeat: 1 }, 0);
-
-gsap.utils.toArray(".quickflip").forEach(flip => {
-    flip.addEventListener("click", event => {
-      tl.play();
-      
-      
-    });
-    // flip.addEventListener("mouseenter", event => tl.play());
-    // flip.addEventListener("mouseleave", event => tl.reverse());
-});
 
 
 

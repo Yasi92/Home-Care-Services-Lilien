d8c4476f7123751298c7bb73df353fbc695c558f
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-09 21:15:26 +0200

    fixing the reverse state of cards by clicking on back button

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,40 +31,55 @@ $(".service-desc").hide();
 
 
 
-// $(".services-link").on('click', function(){
-//     $('.service-short-desc > p, h5, .services-img').hide();
-//     $(".service-desc").fadeIn('slow');
-//     $('.col-toggle').toggleClass('col-lg-8 col-10');
-//     console.log(this);
-// });
+
 
 $(".services-link").on('click', function(event){
-    // $('.service-short-desc > p, h5, .services-img').hide();
-    const button = event.target;
-    const text = event.target.parentElement.nextElementSibling;
-    const clToggle = event.target.parentElement.parentElement;
-    const image = event.target.parentElement.parentElement.previousElementSibling;
+   
+    let button = event.target;
+    let shortDescription = event.target.parentElement.childNodes[3];
+    let text = event.target.parentElement.nextElementSibling;
+    let clToggle = event.target.parentElement.parentElement;
+    let image = event.target.parentElement.parentElement.previousElementSibling;
 
-    console.log(image);
+   
 
     $(button).hide();
     $(image).hide();
     $(text).fadeIn('slow');
     $(clToggle).toggleClass('col-lg-8 col-10');
-    
+    $(shortDescription).hide();
     // $(this).hide()
     // $(".service-desc").fadeIn('slow');
     // $('.col-toggle').toggleClass('col-lg-8 col-10');
     // console.log($(this));
+  
+
+    
 });
 
 
 
 
-$(".back-btn").click(function () {
-    $(".service-desc").hide();
-    $('.service-short-desc > p, h5, .services-img').show();
-    $('.col-toggle').removeClass('col-lg-8 col-10');
+$(".back-btn").on('click' ,function (event) {
+   
+    
+    let text = event.target.parentElement.parentElement;
+    let link = event.target.parentElement.parentElement.previousElementSibling.childNodes[5];
+    let shortDescription = event.target.parentElement.parentElement.previousElementSibling.childNodes[3];
+    let image = event.target.parentElement.parentElement.parentElement.previousElementSibling;
+    let clToggle = event.target.parentElement.parentElement.parentElement;
+   
+console.log(shortDescription);
+    $(text).hide()
+    $(image).show();
+    $(link).show();
+    $(shortDescription).show();
+    $(clToggle).removeClass('col-lg-8 col-10');
+   
+    
+    // $(".service-desc").hide();
+    // $('.service-short-desc > p, h5, .services-img').show();
+    // $('.col-toggle').removeClass('col-lg-8 col-10');
 });
 
 

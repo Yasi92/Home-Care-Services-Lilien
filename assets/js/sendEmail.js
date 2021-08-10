7b845c9ffece9ef0b48b8cdf24c050b1706a663b
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-10 17:55:30 +0200

    add contact form to contact.html and enable this to send form with EmailJs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,6 @@
 
 // The method for sending forms is learned from code institute tutorials.
+///// Sending job forms from job.html
 
 function sendMail(contactForm) {
     emailjs.send("service_knliyzr","template_eyhvnn5", {
@@ -21,3 +22,29 @@ function sendMail(contactForm) {
     );
     return false;  
 }
+
+///// Sending contact forms from contact.html
+function sendContactForm(contactForm) {
+    emailjs.send("service_knliyzr","template_fpcvh9k", {
+        "first-name": contactForm.name.value,
+        "last-name": contactForm.lastname.value,
+        "email": contactForm.email.value,
+        "address": contactForm.address.value,
+        "phone": contactForm.phone.value,
+        "message": contactForm.message.value,
+        "service": contactForm.service.value,
+       
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  
+}
+
+
+

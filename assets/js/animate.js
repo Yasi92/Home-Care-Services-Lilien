406977d8ade5d3a941279a1f129db8426ce522a8
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-11 17:44:38 +0200

    add fade in to viewport animation to services cards in services.html

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -87,3 +87,13 @@ gsap.from("#covid-stats", {
 
 
 }});
+
+
+// the method has been learned from gsap documentaion and video tutorial "https://greensock.com/docs/v3/Plugins/ScrollTrigger/static.batch()"
+ScrollTrigger.batch(".service-row", {
+    onEnter: batch => {
+      gsap.from(batch, {x:-400, duration: 2, stagger:.2})
+    },
+    toggleActions: "restart pause resume pause"
+    
+  })

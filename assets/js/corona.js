edcf616853a78386a53ada883f92e7c58d7771a8
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-18 15:53:27 +0200

    change title, modify texts and add punctuations, add a button in the home page to link to the services page, fix the alignment in contact info section, align card footers in corona news section

--- assets/js/corona.js
+++ assets/js/corona.js
@@ -1,103 +1,90 @@
-// // The method has been learned from this tutorial video on youtube "https://www.youtube.com/watch?v=uxf0--uiX0I&t=1s"
-// async function getData() {
+// The method has been learned from this tutorial video on youtube "https://www.youtube.com/watch?v=uxf0--uiX0I&t=1s"
+async function getData() {
 
-//     const response = await fetch("https://covid-19-data.p.rapidapi.com/country?name=germany&format=json", {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-key": "c627d0f2d2msh9ba409e5b8c495dp17c448jsn5469ec3ad079",
-//             "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
-//         }
-//     });
+    const response = await fetch("https://covid-19-data.p.rapidapi.com/country?name=germany&format=json", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "c627d0f2d2msh9ba409e5b8c495dp17c448jsn5469ec3ad079",
+            "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+        }
+    });
 
-//     const data = await response.json();
-//     const options = await data[0];
-//     console.log(data);
-//     const {
-//         country,
-//         confirmed,
-//         deaths,
-//         recovered,
-//         lastUpdate
-//     } = options;
-
-
-
-//     document.getElementById('country').textContent = country;
-//     document.getElementById('confirmed').textContent = confirmed;
-//     document.getElementById('death').textContent = deaths;
-//     document.getElementById('recovered').textContent = recovered;
-//     document.getElementById('update').textContent = lastUpdate;
-// }
-
-// getData();
+    const data = await response.json();
+    const options = await data[0];
+    console.log(data);
+    const {
+        country,
+        confirmed,
+        deaths,
+        recovered,
+        lastUpdate
+    } = options;
 
 
 
+    document.getElementById('country').textContent = country;
+    document.getElementById('confirmed').textContent = confirmed;
+    document.getElementById('death').textContent = deaths;
+    document.getElementById('recovered').textContent = recovered;
+    document.getElementById('update').textContent = lastUpdate;
+}
 
-
-// async function getNewsData() {
-
-//     const newsResponse = await fetch("https://coronavirus-smartable.p.rapidapi.com/news/v1/DE/", {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-key": "c627d0f2d2msh9ba409e5b8c495dp17c448jsn5469ec3ad079",
-//             "x-rapidapi-host": "coronavirus-smartable.p.rapidapi.com"
-//         }
-//     });
-
-//     const dataNews = await newsResponse.json();
-//     console.log(dataNews.news);
-
-//     var articles = dataNews.news;
-
-//     var ul = `<div class="row my-3 my-md-4">`;
+getData();
 
 
 
 
-//     for (var i = 0; i < articles.length; i++) {
 
-//         const titles = articles[i].title;
-//         const baseURL = articles[i].originalUrl;
-//         const description = articles[i].excerpt;
-//         const providers = articles[i].provider.name;
-//         const published = articles[i].publishedDateTime;
+async function getNewsData() {
 
+    // const newsResponse = await fetch("https://coronavirus-smartable.p.rapidapi.com/news/v1/DE/", {
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-key": "c627d0f2d2msh9ba409e5b8c495dp17c448jsn5469ec3ad079",
+    //         "x-rapidapi-host": "coronavirus-smartable.p.rapidapi.com"
+    //     }
+    // });
 
-//         // ul += '<li class="list-group-item">' + 
-//         // '<a class="corona-links" ' + 'href="' + baseURL + '"' + '>' + titles + '</a>' + '<br>' +
-//         // '<p>' + description + '</p>' + '<br>' +
-//         // '<small>' + providers + '</small>' 
-//         //  + '</li>';
+    const dataNews = await newsResponse.json();
+    console.log(dataNews.news);
 
+    var articles = dataNews.news;
 
-//         ul +=
-//             `<div class=" d-flex col-sm-4 py-3 py-md-4">
-//             <div class="card"> 
-//              <div class="card-body d-flex align-items-start flex-column text-left"> 
-//                  <h5 class="card-title text-left">` + titles + '</h5>'  +
-//                  '<p class="card-text">' + description + '...' + '</p>'  +
-//                  '<div class="d-flex"><small class="text-muted mt-1">' + 'Published:' + published + '</small>' + '</div>'+
-//                 '<div class="mt-auto w-100"><div class="mb-1 d-flex justify-content-end"><a class="btn green-btn" target="_blank" href="' + baseURL + '"' + '>' + "Read More" + '</a></div>'+
-//                 `</div>  </div> </div> </div>`;
+    var ul = `<div class="row my-3 my-md-4">`;
 
+    for (var i = 0; i < articles.length; i++) {
 
-
-
-//         document.getElementById("corona-news").innerHTML = ul;
-        
+        const titles = articles[i].title;
+        const baseURL = articles[i].originalUrl;
+        const description = articles[i].excerpt;
+        const providers = articles[i].provider.name;
+        const published = articles[i].publishedDateTime;
         
 
 
-//     }
+        ul +=
+            `<div class=" d-flex col-sm-4 py-3 py-md-4">
+               <div class="card"> 
+                    <div class="card-body d-flex align-items-start flex-column text-left"> 
+                        <h5 class="card-title text-left">` + titles + '</h5>' +  '<hr>' +                  
+                        '<p class="card-text small-p">' + description + '...' + '</p>' +
+                        '<div class="d-flex"><small class="text-muted mt-1">' + 'Published:' + published + '</small>' + '</div>' +
+                    '</div>' +    
+                    '<div class="mt-auto card-footer w-100"><div class="d-flex justify-content-center"><a class="btn green-btn" target="_blank" href="' + baseURL + '"' + '>' + "Read More" + '</a></div>' +
+                    `</div> 
+                </div> 
+            </div>` ;
+
+        document.getElementById("corona-news").innerHTML = ul;
+    }
 
 
-//     ul += `</div>`;
+    ul += '</div>';
 
-//     console.log(ul);
-// }
+    console.log(ul);
+}
 
-// getNewsData();
+getNewsData();
 
 
 
@@ -105,7 +92,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 gsap.from("#covid-news", {
-    scrollTrigger: {     
+    scrollTrigger: {
         trigger: "#covid-news",
         start: "bottom 90%",
         toggleActions: "restart none none play"
@@ -114,13 +101,15 @@ gsap.from("#covid-news", {
     autoAlpha: 0,
     duration: 3,
     ease: "back",
-    z: 200,
+    z: 300,
     duration: 4,
-    opacity:0,
-    scale:1,
+    opacity: 0,
+    scale: 1,
 });
-ScrollTrigger.addEventListener("refreshInit", () => gsap.set("#covid-news", {autoAlpha: 0}));
- 
+ScrollTrigger.addEventListener("refreshInit", () => gsap.set("#covid-news", {
+    autoAlpha: 0
+}));
+
 gsap.from("#covid-stats", {
     scrollTrigger: {
         trigger: "#covid-stats",
@@ -130,11 +119,13 @@ gsap.from("#covid-stats", {
     autoAlpha: 0,
     duration: 3,
     ease: "back",
-    z: -200,
+    z: -300,
     duration: 4,
-    opacity:0,
-    scale:1,
-    
+    opacity: 0,
+    scale: 1,
+
 });
 
-ScrollTrigger.addEventListener("refreshInit", () => gsap.set("#covid-stats", {autoAlpha: 0}));
+ScrollTrigger.addEventListener("refreshInit", () => gsap.set("#covid-stats", {
+    autoAlpha: 0
+}));

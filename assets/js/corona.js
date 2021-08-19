68ba262ecb3e69d06fedaedecaa6e230c2a9f24c
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-08-19 13:03:54 +0200

    change the layout of corona news boxes based on the viewport size

--- assets/js/corona.js
+++ assets/js/corona.js
@@ -60,7 +60,7 @@ async function getNewsData() {
 
 
         ul +=
-            `<div class=" d-flex col-sm-4 py-3 py-md-4">
+            `<div class=" d-flex col-12 col-md-6 py-3 py-md-4">
                <div class="card"> 
                     <div class="card-body d-flex align-items-start flex-column text-left"> 
                         <h5 class="card-title text-left">` + titles + '</h5>' +  '<hr>' +                  
